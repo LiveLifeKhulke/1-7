842ae8efacd037cda556c6d9dfb01583941bd1c5
Deepak93Gahlot <Deepak.147392@gmail.com>
2025-09-08 13:53:18 +0530

    new commit

--- conftest.py
+++ conftest.py
@@ -1,13 +1,2 @@
 import pytest
-from selenium import webdriver
-
-@pytest.fixture(params=['chrome'],scope='class')
-def setup_module(request):
-    print('This is setup')
-    if request.param == 'chrome':
-    # global driver
-        c_options = webdriver.ChromeOptions()
-        c_options.add_argument("--start-maximized")
-        driver = webdriver.Chrome()
-        request.cls.driver=driver
-        driver.get("https://google.com")
+#
--- test_1.py
+++ test_1.py
@@ -1,5 +1,20 @@
 import pytest
 
+from selenium import webdriver
+import time
+
+@pytest.fixture(params=['chrome'],scope='class')
+def setup_module(request):
+    print('This is setup')
+    if request.param == 'chrome':
+    # global driver
+        c_options = webdriver.ChromeOptions()
+        c_options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(options=c_options)
+        request.cls.driver=driver
+        driver.get("https://google.com")
+        time.sleep(5)
+
 @pytest.mark.usefixtures('setup_module')
 @pytest.mark.smoke1
 @pytest.mark.parametrize('title,url',[('Google','google'),('GOgle','Google')])
