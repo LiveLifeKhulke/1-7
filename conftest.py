a36e29837bc05666c85a553d4c2590684a7b5538
Deepak93Gahlot <Deepak.147392@gmail.com>
2025-09-08 22:43:05 +0530

    new

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ import time
 def setup_module(request):
     print('This is setup')
     if request.param == 'chrome':
-    # global driver
         c_options = webdriver.ChromeOptions()
         c_options.add_argument("--headless")
         driver = webdriver.Chrome(options=c_options)
