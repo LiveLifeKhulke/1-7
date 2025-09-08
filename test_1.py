3b0ab48997f83c5f2bc88de9d5d4d7bdad3e9292
Deepak93Gahlot <Deepak.147392@gmail.com>
2025-09-08 13:04:38 +0530

    new commit

--- test_1.py
+++ test_1.py
@@ -1,10 +1,20 @@
 import pytest
 
-@pytest.mark.usefixtures('setup_module')
+# @pytest.mark.usefixtures('setup_module')
+# @pytest.mark.smoke1
+# @pytest.mark.parametrize('title,url',[('Google','google'),('GOgle','Google')])
+# class Test_demo:
+#     def test_demo(self,title,url):
+#         print('Hi , this is test')
+#         assert title in self.driver.title
+#         assert url in self.driver.current_url
+
+import requests
+
 @pytest.mark.smoke1
-@pytest.mark.parametrize('title,url',[('Google','google'),('GOgle','Google')])
-class Test_demo:
-    def test_demo(self,title,url):
-        print('Hi , this is test')
-        assert title in self.driver.title
-        assert url in self.driver.current_url
+def test_demo():
+    url = 'https://fake-json-api.mock.beeceptor.com/user'
+    response = requests.get(url)
+    json_response=response.json()
+    assert not response.status_code == 201
+    print(json_response['paths'][2])
