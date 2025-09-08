0b19feb5d78ba8bace352ba9466290e7fdc42c52
Deepak93Gahlot <Deepak.147392@gmail.com>
2025-09-08 22:05:50 +0530

    with new yaml

--- conftest.py
+++ conftest.py
@@ -1,2 +1,16 @@
 import pytest
-#
+
+from selenium import webdriver
+import time
+
+@pytest.fixture(params=['chrome'],scope='class')
+def setup_module(request):
+    print('This is setup')
+    if request.param == 'chrome':
+    # global driver
+        c_options = webdriver.ChromeOptions()
+        c_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=c_options)
+        request.cls.driver=driver
+        driver.get("https://google.com")
+        time.sleep(5)
