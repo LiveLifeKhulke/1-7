7a0fffbfafe110fb2bfe6ad119e33f528b87378d
Deepak93Gahlot <Deepak.147392@gmail.com>
2025-09-15 09:54:31 +0530

    15 commit

--- test_1.py
+++ test_1.py
@@ -2,6 +2,7 @@ import pytest
 
 @pytest.mark.usefixtures('setup_teardown')
 @pytest.mark.parametrize('title',[('google'),('gooGle')])
+@pytest.mark.smoke2
 class Test_demo:
     def test_demo(self,title):
         assert self.driver.title in title
